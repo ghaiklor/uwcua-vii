1f49369962493b6f8385a6e28a74bddd8ff5cbfe
Eugene Obrezkov <ghaiklor@gmail.com>
2015-03-23 13:31:47 +0200

    Done with CLI

--- bin/gitter-bot.js
+++ bin/gitter-bot.js
@@ -1,9 +1,14 @@
 var program = require('commander');
 var pkg = require('../package.json');
+var GitterBot = require('../lib/GitterBot');
 
 program
   .version(pkg.version)
   .description('Gitter bot for evaluating expressions')
-  .usage('[options] <room-name>')
-  .option('-k, --key', 'Override API key by default')
+  .usage('[options]')
+  .option('-k, --key <key>', 'Override default API key')
+  .option('-r, --room <room>', 'Override default room')
   .parse(process.argv);
+
+var bot = new GitterBot({key: program.key, room: program.room});
+process.on('exit', bot.destroy.bind(bot));
