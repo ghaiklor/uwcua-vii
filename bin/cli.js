6c03135d4cc037cbbbaff46dd1655cc5e4150e94
Eugene Obrezkov <ghaiklor@gmail.com>
2015-03-23 11:32:22 +0200

    Add node-gitter to deps

--- bin/cli.js
+++ bin/cli.js
@@ -3,5 +3,10 @@ var pkg = require('../package.json');
 
 program
   .version(pkg.version)
+  .description('Gitter bot for evaluating expressions')
   .usage('<room-name>')
   .parse(process.argv);
+
+if (!program.args[0]) {
+  throw new Error('You must supply room');
+}
