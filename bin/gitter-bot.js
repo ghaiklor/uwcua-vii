24aaf9f9c592da9e9527aef039c54cba72822668
Eugene Obrezkov <ghaiklor@gmail.com>
2015-03-23 14:03:57 +0200

    Huge cleaning up

--- bin/gitter-bot.js
+++ bin/gitter-bot.js
@@ -6,9 +6,9 @@ program
   .version(pkg.version)
   .description('Gitter bot for evaluating expressions')
   .usage('[options]')
-  .option('-k, --key <key>', 'Override default API key')
-  .option('-r, --room <room>', 'Override default room')
+  .option('-k, --key <key>', 'Set API key')
+  .option('-r, --room <room>', 'Set room')
   .parse(process.argv);
 
-var bot = new GitterBot({key: program.key, room: program.room});
+var bot = new GitterBot({apiKey: program.key, roomName: program.room});
 process.on('exit', bot.destroy.bind(bot));
